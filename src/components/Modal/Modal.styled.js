67ee2057609d87e0dcceb08075145fb2412acb0c
”John <102276922+UlianaNad@users.noreply.github.com>
2024-02-23 13:50:31 +0200

    fixing height and scroll up

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -12,7 +12,7 @@ export const StyledOverlay = styled.div`
   align-items: center;
   background-color: rgba(0, 0, 0, 0.8);
   z-index: 1200;
-  height: -webkit-fill-available;
+  /* height: -webkit-fill-available; */
 `;
 
 export const StyledModal = styled.div`
@@ -21,7 +21,7 @@ export const StyledModal = styled.div`
   background-color: white;
   overflow-y: scroll;
   position: relative;
-  height: -webkit-fill-available;
+  /* height: -webkit-fill-available; */
   //padding: 20px 30px;
 
   /* @media (max-width: 968px) {
