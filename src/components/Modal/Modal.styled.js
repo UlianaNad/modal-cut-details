4e2944fa3bdf9b3c1ee53eeec9c015aa6b070573
”John <102276922+UlianaNad@users.noreply.github.com>
2024-02-26 16:42:46 +0200

    df

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -16,7 +16,8 @@ export const StyledOverlay = styled.div`
 
 export const StyledModal = styled.div`
   width: 100lvw;
-  height: 100lvh;
+  //  height: 100vh; /* Fallback for browsers that do not support Custom Properties */
+  height: calc(var(--vh, 1vh) * 100);
   background-color: white;
   overflow-y: scroll;
   position: relative;
